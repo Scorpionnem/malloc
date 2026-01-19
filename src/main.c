8e26c43cdf54f9bbe6b6dd175bd70a3a2130c879
Mewen Batty <scorpionem86@gmail.com>
2026-01-19 14:36:39 +0100

    working zone appending and creation, just need free and realloc? hopefully so

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mbatty <mbatty@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 10:09:42 by mbatty            #+#    #+#             */
-/*   Updated: 2026/01/19 13:57:13 by mbatty           ###   ########.fr       */
+/*   Updated: 2026/01/19 14:32:45 by mbatty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,53 @@
 
 t_malloc	g_malloc = {0};
 
+/*
+	Finds/Allocate a zone
+
+	@param zone zone group to find an available zone in
+	@param size size of the user's allocation
+
+	@return pointer to the adress of the created/found node or NULL on failure
+*/
+t_zone	**find_available_zone(t_zone **zone, size_t size)
+{
+	if (!*zone)
+	{
+		*zone = new_zone(size); // Initialize NULL zone
+		if (!*zone)
+			return (NULL);
+		return (zone);
+	}
+	t_zone	**tmp_zone = zone;
+	t_zone	**prev_tmp_zone = NULL;
+	while ((*tmp_zone))
+	{
+		t_block	*tmp_block = (*tmp_zone)->blocks;
+
+		while (tmp_block)
+		{
+			if (!tmp_block->used)
+			{
+				return (tmp_zone); // Zone is available
+			}
+			tmp_block = tmp_block->next;
+		}
+		prev_tmp_zone = tmp_zone;
+		tmp_zone = &(*tmp_zone)->next;
+	}
+	(*prev_tmp_zone)->next = new_zone(size);
+	if (!(*prev_tmp_zone)->next)
+		return (NULL);
+	return (&(*prev_tmp_zone)->next); // Append new zone to list
+}
+
+/*
+	Returns the right zone group for size
+
+	@param size size of the user's allocation
+
+	@return pointer to the adress to the first zone
+*/
 t_zone	**get_zone(size_t size)
 {
 	if (size <= SMALL_ALLOC_SIZE)
@@ -56,15 +103,12 @@ t_zone	**get_zone(size_t size)
 void	*ft_malloc(size_t size)
 {
 	t_zone	**zone = get_zone(size);
+	zone = find_available_zone(zone, size);
 	if (!*zone)
-	{
-		*zone = new_zone(size);
-		if (!*zone)
-			return (NULL);
-	}
+		return (NULL);
 
 	t_block	*res = find_block(*zone, size);
-	if (!res)
+	if (!res) // If this happens, wtf?!
 		return (NULL);
 	res->used = true;
 	return (res->adress);
@@ -72,9 +116,11 @@ void	*ft_malloc(size_t size)
 
 int	main(void)
 {
-	char	*caca = ft_malloc(64);
-	caca = ft_malloc(16);
-	caca = ft_malloc(500);
+	ft_malloc(16);
+	ft_malloc(16);
+	ft_malloc(64);
+	ft_malloc(500);
+	ft_malloc(420);
 
 	print_zones("SMALL", g_malloc.small_zones);
 	printf("\n");

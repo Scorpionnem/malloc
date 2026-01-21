ce0078787a24fa7b16e5b4580c61c17aa992ecb7
mewen <scorpionem86@gmail.com>
2026-01-21 21:16:38 +0100

    should be a valid realloc lol

--- src/realloc.c
+++ src/realloc.c
@@ -6,7 +6,7 @@
 /*   By: mbatty <mbatty@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 16:57:42 by mbatty            #+#    #+#             */
-/*   Updated: 2026/01/21 17:40:33 by mbatty           ###   ########.fr       */
+/*   Updated: 2026/01/21 21:16:19 by mbatty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,15 @@ void	*realloc(void *ptr, size_t size)
 		free(ptr);
 		return (NULL);
 	}
-	// t_block	*block = find_block_by_adress_global(ptr);
-	
-	return (NULL);
+	t_block	*block = find_block_by_adress_global(ptr);
+	if (!block)
+		return (NULL);
+	void	*blk = (void*)block + BLOCK_HEADER_SIZE;
+	void	*res = malloc(size);
+	if (!res)
+		return (NULL);
+	size_t	move_size = block->used_size > size ? size : block->used_size;
+	memmove(res, blk, move_size);
+	free(blk);
+	return (res);
 }

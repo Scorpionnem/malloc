dfae9643a32fee5b5af4c7d90ad07bb95afe31a6
Mewen Batty <scorpionem86@gmail.com>
2026-01-22 14:02:16 +0100

    cool compiler, yeah

--- src/realloc.c
+++ src/realloc.c
@@ -6,11 +6,16 @@
 /*   By: mbatty <mbatty@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 16:57:42 by mbatty            #+#    #+#             */
-/*   Updated: 2026/01/21 21:16:19 by mbatty           ###   ########.fr       */
+/*   Updated: 2026/01/22 14:01:05 by mbatty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_malloc_internal.h"
+
+static void	dumb_compiler(void *ptr)
+{
+	free(ptr);
+}
 
 void	*realloc(void *ptr, size_t size)
 {
@@ -30,6 +35,6 @@ void	*realloc(void *ptr, size_t size)
 		return (NULL);
 	size_t	move_size = block->used_size > size ? size : block->used_size;
 	memmove(res, blk, move_size);
-	free(blk);
+	dumb_compiler(blk);
 	return (res);
 }
